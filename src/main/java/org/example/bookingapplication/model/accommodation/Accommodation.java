5aeaf2b639de69c517d117cbc600e1d769f27964
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2025-03-12 08:09:29 +0200

    added improvements

--- src/main/java/org/example/bookingapplication/model/accommodation/Accommodation.java
+++ src/main/java/org/example/bookingapplication/model/accommodation/Accommodation.java
@@ -1,7 +1,17 @@
 package org.example.bookingapplication.model.accommodation;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.Set;
 import lombok.EqualsAndHashCode;
